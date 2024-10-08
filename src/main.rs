849efc8b927046230ab34af6b8888dce16151daa
Shashank P <shashankp2832@gmail.com>
2024-10-08 20:12:05 +0530

    Created a board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+pub struct Board {
+    pub white_rooks : u64,
+    pub white_knights : u64,
+    pub white_bishops : u64,
+    pub white_queens : u64,
+    pub white_king : u64,
+    pub white_pawns : u64,
+
+    pub black_rooks : u64,
+    pub black_knights : u64,
+    pub black_bishops : u64,
+    pub black_queens : u64,
+    pub black_king : u64,
+    pub black_pawns : u64,
+
+    pub en_passant : u16,
+    pub castling_rights : u8,
+}
+
 fn main() {
     println!("Hello, world!");
 }
