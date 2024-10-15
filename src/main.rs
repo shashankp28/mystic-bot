21c2f062a96002c837013f82d181996bb730cdd9
Shashank P <shashankp2832@gmail.com>
2024-10-15 22:56:26 +0530

    Fixed update tickers issue and added a new test for queens

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use mystic_bot::base::defs::Board;
 use std::time::Instant;
+use std::fs;
 
 fn generate_game_tree( curr_board: Board, max_depth: u32, num_nodes: &mut u64 ) {
 
@@ -12,37 +13,57 @@ fn generate_game_tree( curr_board: Board, max_depth: u32, num_nodes: &mut u64 ) 
     }
 }
 
+fn generate_hash_for_boards() -> std::io::Result<()> {
+    let path = "./sample/test/queen_test/";
+    for entry in fs::read_dir(path)? {
+        match entry {
+            Ok(file_path) => match Board::from_file(file_path.path()) {
+                Ok(board) => {
+                    let hash = board.hash();
+                    println!("{hash}")
+                }
+                Err(e) => {
+                    eprintln!("Error loading the board {}", e)
+                }
+            },
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    let file_path = "sample/start.json";
-    let mut curr_board: Option<Board> = Option::None;
-    match Board::from_file( file_path ) {
-        Ok( board ) => {
-            curr_board = Some( board );
-            println!( "Successfully loaded board: {:?}", board );
-            let legal_moves: Vec<Board> = board.get_legal_moves();
-            for (i, new_board) in legal_moves.iter().enumerate() {
-                let filename = format!("sample/{}.json", i);
-                new_board.save_board(&filename);
-            }
-        }
-        Err( e ) => {
-            println!( "Error loading board: {}", e );
-        }
-    }
+    // let file_path = "sample/start.json";
+    // let mut curr_board: Option<Board> = Option::None;
+    // match Board::from_file( file_path ) {
+    //     Ok( board ) => {
+    //         curr_board = Some( board );
+    //         println!( "Successfully loaded board: {:?}", board );
+    //         let legal_moves: Vec<Board> = board.get_legal_moves();
+    //         for (i, new_board) in legal_moves.iter().enumerate() {
+    //             let filename = format!("sample/{}.json", i);
+    //             new_board.save_board(&filename);
+    //         }
+    //     }
+    //     Err( e ) => {
+    //         println!( "Error loading board: {}", e );
+    //     }
+    // }
 
-    if let Some(board) = curr_board {
-        let max_depth = 5;
-        let mut num_nodes: u64 = 0;
+    // if let Some(board) = curr_board {
+    //     let max_depth = 5;
+    //     let mut num_nodes: u64 = 0;
 
-        let start_time = Instant::now();
-        generate_game_tree(board, max_depth, &mut num_nodes);
-        let duration = start_time.elapsed();
-        let duration_secs = duration.as_secs_f64();
+    //     let start_time = Instant::now();
+    //     generate_game_tree(board, max_depth, &mut num_nodes);
+    //     let duration = start_time.elapsed();
+    //     let duration_secs = duration.as_secs_f64();
 
-        println!("Number of Nodes Traversed: {}", num_nodes);
-        println!("Time Taken: {:.2} seconds", duration_secs);
-        println!("Nodes per second: {:.2}", num_nodes as f64 / duration_secs);
-    } else {
-        println!("Failed to load the board, exiting.");
-    }
+    //     println!("Number of Nodes Traversed: {}", num_nodes);
+    //     println!("Time Taken: {:.2} seconds", duration_secs);
+    //     println!("Nodes per second: {:.2}", num_nodes as f64 / duration_secs);
+    // } else {
+    //     println!("Failed to load the board, exiting.");
+    // }
+    generate_hash_for_boards();
 }
