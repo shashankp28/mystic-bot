609660410f678207bed6b8a7dc1835c955d74980
Shashank P <shashankp2832@gmail.com>
2024-10-19 02:14:07 +0530

    Changed Search depth to 6 from 15!

--- src/bot/search.rs
+++ src/bot/search.rs
@@ -123,7 +123,7 @@ impl Search {
         } else {
             f64::INFINITY
         };
-        let depth = 15;
+        let depth = 6;
         for next_board in self.board.get_legal_moves() {
             self.num_nodes += 1;
             let eval = self.alpha_beta_pruning(
