3363ec1bb5829b74019bbe9fd0dfb95f58848671
Anand <anandishegde@gmail.com>
2024-10-19 00:02:25 +0530

    Add ordering of nodes based on evaluation function

--- src/bot/search.rs
+++ src/bot/search.rs
@@ -1,20 +1,26 @@
-use crate::base::defs::{Board, Search, GameState};
+use crate::base::defs::{Board, GameState, Search};
+use std::cmp::Ordering;
 use std::time::Instant;
 
-pub fn generate_game_tree( curr_board: Board, max_depth: u32, num_nodes: &mut u64 ) {
+pub fn generate_game_tree(curr_board: Board, max_depth: u32, num_nodes: &mut u64) {
     *num_nodes += 1;
     if max_depth == 0 {
         return;
     }
     for board in curr_board.get_legal_moves() {
-        generate_game_tree(board, max_depth-1, num_nodes);
+        generate_game_tree(board, max_depth - 1, num_nodes);
     }
 }
 
 impl Search {
-
-    pub fn alpha_beta_pruning(&mut self, board: Board, mut alpha: f64, mut beta: f64, depth: u32, maximizing_player: bool) -> f64 {
-        
+    pub fn alpha_beta_pruning(
+        &mut self,
+        board: Board,
+        mut alpha: f64,
+        mut beta: f64,
+        depth: u32,
+        maximizing_player: bool,
+    ) -> f64 {
         //  Get cached eval score
         let board_hash = board.hash();
         if self.memory.contains_key(&board_hash) {
@@ -30,7 +36,7 @@ impl Search {
 
         // If checkmate or draw return appropriate score
         let mut legal_moves = board.get_legal_moves();
-        let is_black: u8 = if ( board.metadata >> 8 ) & 1 == 1 { 0 } else { 1 };
+        let is_black: u8 = if (board.metadata >> 8) & 1 == 1 { 0 } else { 1 };
         let game_state = if legal_moves.len() == 0 {
             let king_positions: u64 = (board.kings >> (64 * is_black)) as u64;
             let pos: i8 = king_positions.trailing_zeros() as i8;
@@ -45,15 +51,44 @@ impl Search {
         };
         match game_state {
             GameState::Checkmate => {
-                if is_black==1 {
+                if is_black == 1 {
                     return 100000.0;
                 } else {
                     return -100000.0;
                 }
-            },
+            }
             GameState::Stalemate => return 0.0,
             GameState::Playable => {}
         }
+
+        legal_moves.data.sort_by(|a, b| {
+            let a_evaluation;
+            let b_evaluation;
+
+            if self.memory.contains_key(&a.hash()) {
+                a_evaluation = *self.memory.get(&board_hash).unwrap();
+            } else {
+                a_evaluation = board.evaluate();
+                self.memory.insert(a.hash(), a_evaluation);
+            }
+
+            if self.memory.contains_key(&b.hash()) {
+                b_evaluation = *self.memory.get(&b.hash()).unwrap();
+            } else {
+                b_evaluation = board.evaluate();
+                self.memory.insert(b.hash(), b_evaluation);
+            }
+
+            let order = a_evaluation
+                .partial_cmp(&b_evaluation)
+                .unwrap_or(Ordering::Equal);
+
+            if is_black == 1 {
+                order // Ascending for black
+            } else {
+                order.reverse() // Descending for white
+            }
+        });
 
         if maximizing_player {
             let mut max_eval = f64::NEG_INFINITY;
@@ -84,30 +119,45 @@ impl Search {
 
     pub fn best_next_board(&mut self) -> Option<Board> {
         let start_time = Instant::now();
-        let is_black: i32 = if ( self.board.metadata >> 8 ) & 1 == 1 { 0 } else { 1 };
+        let is_black: i32 = if (self.board.metadata >> 8) & 1 == 1 {
+            0
+        } else {
+            1
+        };
         let mut best_move = None;
-        let mut best_eval = if is_black==0 { f64::NEG_INFINITY } else { f64::INFINITY };
+        let mut best_eval = if is_black == 0 {
+            f64::NEG_INFINITY
+        } else {
+            f64::INFINITY
+        };
         let depth = 5;
         for next_board in self.board.get_legal_moves() {
             self.num_nodes += 1;
-            let eval = self.alpha_beta_pruning(next_board, f64::NEG_INFINITY, f64::INFINITY, depth - 1, is_black==1);
-            
+            let eval = self.alpha_beta_pruning(
+                next_board,
+                f64::NEG_INFINITY,
+                f64::INFINITY,
+                depth - 1,
+                is_black == 1,
+            );
+
             if is_black == 0 && eval > best_eval {
                 best_eval = eval;
                 best_move = Some(next_board);
-            }
-            else if is_black == 1 && eval < best_eval {
+            } else if is_black == 1 && eval < best_eval {
                 best_eval = eval;
                 best_move = Some(next_board);
             }
         }
-        
+
         let elapsed_time = start_time.elapsed();
-        println!( "Evaluation Function: {}", best_eval );
-        println!( "Number of Nodes Explored: {}", self.num_nodes );
-        println!( "Time Taken: {:?}", elapsed_time );
-        println!( "Explored Nodes per second: {}", self.num_nodes as f64 / elapsed_time.as_secs_f64() );
+        println!("Evaluation Function: {}", best_eval);
+        println!("Number of Nodes Explored: {}", self.num_nodes);
+        println!("Time Taken: {:?}", elapsed_time);
+        println!(
+            "Explored Nodes per second: {}",
+            self.num_nodes as f64 / elapsed_time.as_secs_f64()
+        );
         best_move
     }
-
 }
